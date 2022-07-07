2a537fde423a15a3510fce987ce207f7a7833011
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-07 17:12:45 +0500

    Controller Namespaces [2.6]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   get "authors", to: "publishers#index"
   get "public", action: :index, controller: 'publishers'
   resources :publishers, :books
+  namespace :admin do
+    resources :articles
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_112656) do
+ActiveRecord::Schema.define(version: 2022_07_07_113929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
