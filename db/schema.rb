54d9b5d5362645cc6b07652bb6e867270d3a3cef
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-07 16:28:30 +0500

    [multiple resources]define at once in raoutes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_111229) do
+ActiveRecord::Schema.define(version: 2022_07_07_112656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "publishers", force: :cascade do |t|
     t.string "name"
