c1e244f6a3932ed0e7637548b31444a58fcb3a43
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-14 18:59:21 +0500

    Added On: and allow_blank & allow_nil validation

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,4 @@
 class Publisher < ApplicationRecord
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, on: :create, allow_blank: true
 end
