a5046c96ff520ef59ee1a59c01b7fe1db4fa173a
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-14 18:51:45 +0500

    Added uniqueness,numbericaly & Length validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
-  validates :name, presence: true
+   validates :name, presence: true, length: { in: 6..12 }
+   validates :price, numericality: true
+  # validates :name, presence: true, length: { maximum: 10 }
+  #  validates :name, presence: true, length: { minimum: 5 }
+  # validates :name, presence: true, length: { is: 5 }
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,3 +1,4 @@
 class Publisher < ApplicationRecord
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :email, uniqueness: true
 end
