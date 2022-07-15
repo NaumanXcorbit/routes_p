5d3d3e63ef580595e3cc93c72022cf00c886b93c
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-15 12:37:20 +0500

    Around destroy callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  around_destroy :show_delete_info
+
+  def show_delete_info
+    puts "Your parent post #{self.post.title} in destroying"
+    yield
+  end
 end
