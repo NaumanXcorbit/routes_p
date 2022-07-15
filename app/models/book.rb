5664eaa395f7f375c1d298c838dc76e6eb08f17f
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-15 10:34:35 +0500

    before_validation & and after_validation callback

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,14 @@ class Book < ApplicationRecord
   # validates :name, presence: true, length: { maximum: 10 }
   #  validates :name, presence: true, length: { minimum: 5 }
   # validates :name, presence: true, length: { is: 5 }
+   before_validation :remove_white_space
+   after_validation :name_capitalize
+
+   def remove_white_space
+      self.name.strip!
+   end
+
+   def name_capitalize
+      self.name.capitalize!
+   end
 end
