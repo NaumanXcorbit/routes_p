5c7641ef57c1d4e220253119ed6633e8e7fba7ac
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-15 12:45:45 +0500

    after commit callback

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   has_one_attached :image, :dependent => :destroy
+  after_commit :show_details
+
+  def show_details
+    puts "#{self.title} have some changing right now"
+  end
 end
