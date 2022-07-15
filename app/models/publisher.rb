abd92c6894eb64ce9691f93212e614dc32f92d8d
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-15 10:26:12 +0500

    after_initialize,after_find & after_touch callback

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,22 @@
 class Publisher < ApplicationRecord
-  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :name, presence: { strict: true }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :email, uniqueness: true, on: :create, allow_blank: true
+  after_initialize :show_name
+  after_find :show_email
+
+  def show_name
+    if self.name.present?
+      puts "Welcome #{name}-----"
+    end
+  end
+
+  def show_email
+    if self.email.present?
+      puts "publisher email is #{email}"
+    end
+  end
+
+  after_touch do
+    puts "publisher have touched an object"
+  end
 end
